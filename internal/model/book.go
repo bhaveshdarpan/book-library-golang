10719b1f2df73888d30f105f2a153785b742731e
bhakti-letsbloom <Bhakti.Chouksey@letsbloom.io>
2024-08-22 15:58:46 +0530

    commit

---
+++ internal/model/book.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"github.com/pborman/uuid"
+	"gorm.io/gorm"
+)
+
+type Book struct{
+	ID uuid         `gorm:"primaryKey;autoincrement" json:"id"` 
+	Title string    `gorm:"column:Book_Title" json:"title"`
+	Author string   `gorm:"column:Author" json:"author"` 
+	Genre string    `gorm:"column:Genre" json:"genre"`
+	Quantity int    `gorm:"column:quantity" json:"quantity"` 
+}
+
+func (Book) TableName() string{
+	return "BooksLibrary"
+}
