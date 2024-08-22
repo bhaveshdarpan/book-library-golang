05449502184aa434bbf351e1fb60c0c17a58979a
Bhavesh Darpan <bhavesh.darpan@letsbloom.io>
2024-08-22 17:21:59 +0530

    mod updates

--- internal/model/book.go
+++ internal/model/book.go
@@ -1,5 +1,16 @@
 package model
 
+<<<<<<< Updated upstream
+=======
+import (
+	"github.com/google/uuid"
+)
+
+>>>>>>> Stashed changes
 type Book struct {
 
+<<<<<<< Updated upstream
 }
+=======
+const TableName = "BooksLibrary"
+>>>>>>> Stashed changes
